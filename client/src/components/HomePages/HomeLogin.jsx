7a585d7385831681813a158a7bcf0276af9535b3
May <may0000ff@gmail.com>
2019-09-07 12:11:37 -0500

    created home pages

---
+++ client/src/components/HomePages/HomeLogin.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class HomeLogin extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+
+  }
+
+  render() {
+    return (
+      <div>
+        Login
+      </div>
+    )
+  }
+}
+
+export default HomeLogin;
---
+++ client/src/components/HomePages/Tutorial.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Tutorial extends React.Component {
+  render() {
+    return (
+      <div>
+        Tutorial
+      </div>
+    )
+  }
+}
+
+export default Tutorial;
