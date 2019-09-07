3860ff8fd7cbf8a5374bd7a56a8220dc9aefc13c
May <may0000ff@gmail.com>
2019-09-07 12:12:44 -0500

    created navbar for homepages

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { BrowserRouter as  Router, Route, Link } from 'react-router-dom';
+import { browserHistory } from 'react-router';
+import HomeLogin from './HomePages/HomeLogin.jsx';
+import About from './HomePages/About.jsx';
+import Tutorial from './HomePages/Tutorial.jsx';
+import Navbar from './HeaderComponent/Navbar.jsx'; 
 
 class App extends React.Component {
   constructor(props) {
@@ -10,12 +16,17 @@ class App extends React.Component {
   }
 
   render() {
-
     return (
-      <div>
-
-      </div>  
-    )}
+      <Router>
+        <div>
+          <Navbar />
+          <Route name="login" exact path="/" component={HomeLogin} />
+          <Route name="about" exact path="/About" component={About} />
+          <Route name="tutorial" exact path="/Tutorial" component={Tutorial} />
+        </div>  
+      </Router>
+    )
+  }
 }
 
 export default App;
