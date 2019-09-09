1ff905a522ff023dad75c96050e72aad59736458
Nicholas Kolodziej <kolonick@gmail.com>
2019-09-09 10:57:52 -0500

    imported axios

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { BrowserRouter as  Router, Route, Link } from 'react-router-dom';
 import { browserHistory } from 'react-router';
 import HomeLogin from './HomePages/HomeLogin.jsx';
