019e4356b2e375039bf76e41542b05edc921d001
gmarinojr <garymarinojr@gmail.com>
2019-09-09 11:14:40 -0500

    added name field

--- client/src/components/HomePages/HomeLogin.jsx
+++ client/src/components/HomePages/HomeLogin.jsx
@@ -12,7 +12,14 @@ class HomeLogin extends React.Component {
   render() {
     return (
       <div>
-        Login
+      Login
+        <form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
       </div>
     )
   }
