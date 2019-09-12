f6295850c10f09228bd9b483e65c41206d45fe21
Nicholas Kolodziej <kolonick@gmail.com>
2019-09-12 15:43:32 -0500

    added more side nav buttons

--- client/src/components/SidebarComponents/Sidebar.jsx
+++ client/src/components/SidebarComponents/Sidebar.jsx
@@ -4,10 +4,15 @@ import { Link } from 'react-router-dom';
 class Sidebar extends React.Component {
   render() {
     return (
-      <div>
-        <ul id="sideButtons">
+      <div class='sidebar'>
+        <ul class="sideButtons">
+          <li className="sideNavButton"><Link to="Board">Board</Link></li>
+          <li className="sideNavButton"><Link to="Tenants">Tenants</Link></li>
           <li className="sideNavButton"><Link to="Deposit">Deposit</Link></li>
           <li className="sideNavButton"><Link to="Expense">Expense</Link></li>
+          <li className="sideNavButton"><Link to="View">View</Link></li>
+          <li className="sideNavButton"><Link to="View">Settings</Link></li>
+          
         </ul>
       </div>
     )
