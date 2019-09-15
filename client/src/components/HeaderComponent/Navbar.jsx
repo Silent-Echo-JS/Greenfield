c067fc5141b0e5774c423f36180d662985d2db0b
Nicholas Kolodziej <kolonick@gmail.com>
2019-09-15 04:43:10 -0500

    formatting, added image and about

--- client/src/components/HeaderComponent/Navbar.jsx
+++ client/src/components/HeaderComponent/Navbar.jsx
@@ -8,16 +8,39 @@ class NavBar extends React.Component {
   render() {
     return (
       <Router>
-      <div class='title'><h1>Simple HOA</h1></div>
-        <div class='header'>
+        <center><br /><br />
+          <div class='title'>
+            <center><img class='imgsplash' src="https://i.ibb.co/J2skB7Y/logo.png" />
+              <br /><h1>Simple HOA</h1></center><br />
+            <div class='about'>
+              <h2>Introducing</h2><br />
+              <h3>A home owner association management system<br />
+                with the basic features to keep your workspace<br />
+                simple and easy to update and maintain.</h3><br /><br />
+
+              <h2>Manage</h2><br /><h3>Board members, tenants, deposits and expenses<br />
+              for which you can choose the categories on how you see fit.</h3>
+            </div>
+            </div>
+          <div class='login'>
+            <Route name="Login" exact path="/" component={Login} />
+            <Route name="Register" exact path="/Register" component={Register} />
+            </div>
+            <br /><br />
+            <div class='footer'>
+            Simple HOA 2019<br />
+            Programming & Design by SilentEcho JS<br />
+            Nick Kolodziej, Gary Marino, & May Do
+          </div>
+        </center>
+        {/* <div class='header'>
           <ul class="headerButtons">
             <li className="navButton"><Link to="About">About</Link></li>
             <li className="navButton"><Link to="">Home</Link></li>
           </ul>
-        </div>
-        <Route name="login" exact path="/" component={Login} />
-        <Route name="about" exact path="/About" component={About} />
-        <Route name="Register" exact path="/Register" component={Register} />
+        </div> */}
+        {/*  */}
+        {/* <Route name="about" exact path="/About" component={About} /> */}
       </Router>
     )
   }
