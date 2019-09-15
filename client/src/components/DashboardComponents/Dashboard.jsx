f1ef52471ecb1321242ac4be9a420c93502f62a6
Nicholas Kolodziej <kolonick@gmail.com>
2019-09-14 19:32:07 -0500

    imported board

--- client/src/components/DashboardComponents/Dashboard.jsx
+++ client/src/components/DashboardComponents/Dashboard.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Deposit from '../Deposit.jsx';
 import Expense from '../Expense.jsx';
 import Tenants from '../Tenants.jsx';
+import Board from '../Board.jsx';
 
 class Dashboard extends React.Component {
   render() {
@@ -22,6 +23,7 @@ class Dashboard extends React.Component {
         <Route name="Deposit" exact path="/Deposit" component={Deposit} />
         <Route name="Expense" exact path="/Expense" component={Expense} />
         <Route name="Tenants" exact path="/Tenants" component={Tenants} />
+        <Route name="Board" exact path="/Board" component={Board} />
       </Router>
     )
   }
