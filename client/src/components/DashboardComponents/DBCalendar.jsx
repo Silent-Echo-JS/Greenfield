3af194f0f261396acd27ec40c06dc1f00b11efbe
Dan Murphy <dmurphy79@gmail.com>
2019-09-16 16:27:01 -0500

    Adds reactstrap, dashboard components

---
+++ client/src/components/DashboardComponents/DBCalendar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DBCalendar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default DBCalendar;
