4cefac96d794dca81cfcf6497fa1b0edc33ad17e
gmarinojr <garymarinojr@gmail.com>
2019-09-16 00:59:00 -0500

    redirects to dashboard

--- client/src/components/HomePages/Register.jsx
+++ client/src/components/HomePages/Register.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
+// import axios from "axios";
 
 class Register extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      first_name: "",
-      last_name: "",
+      firstname: "",
+      lastname: "",
       username: "",
       password: "",
-      errors: {}
+      toDashboard: false
     };
 
     this.onChange = this.onChange.bind(this);
@@ -18,78 +20,102 @@ class Register extends React.Component {
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
-  onSubmit(e) {
-    e.preventDefault();
 
-    const newUser = {
-      first_name: this.state.first_name,
-      last_name: this.state.last_name,
-      username: this.state.email,
-      password: this.state.password
-    };
-
-    register(newUser).then(res => {
-      this.props.history.push('/');
-    });
+  onSubmit() {
+    this.setState(() => ({
+      toDashboard: true
+    }));
   }
 
+  // const newUser = {
+  //   firstname: this.state.firstname,
+  //   lastname: this.state.lastname,
+  //   username: this.state.email,
+  //   password: this.state.password
+  // };
+
+  // const register = newUser =>
+  //   axios
+  //     .post("/user/register", {
+  //       firstname: newUser.first_name,
+  //       lastname: newUser.last_name,
+  //       email: newUser.email,
+  //       password: newUser.password
+  //     })
+  //     .then(() => {
+  //       console.log("Registered");
+  //     });
+
+  // register(newUser).then(res => {
+  //   this.props.history.push("/register");
+  // });
+
   render() {
+    const { toDashboard, firstname, lastname, username, password } = this.state;
+    if (toDashboard === true) {
+      return <Redirect to="/dashboard" />;
+    }
     return (
-          <div className="col-md-6 mt-5 mx-auto">
-            <form noValidate onSubmit={this.onSubmit}>
-              <h1 className="h3 mb-3 font-weight-normal">Register</h1>
-              <div className="form-group">
-                <label htmlFor="name">First name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="first_name"
-                  placeholder="Enter your first name"
-                  value={this.state.first_name}
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="name">Last name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="last_name"
-                  placeholder="Enter your lastname name"
-                  value={this.state.last_name}
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  name="username"
-                  placeholder="Enter username"
-                  value={this.state.username}
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  name="password"
-                  placeholder="Password"
-                  value={this.state.password}
-                  onChange={this.onChange}
-                />
-              </div>
-              <button
-                type="submit"
-                className="btn btn-lg btn-primary btn-block"
-              >
-                Register!
-              </button>
-            </form>
+      <div className="col-md-6 mt-5 mx-auto">
+        <form noValidate onSubmit={this.onSubmit}>
+          <h1 className="h3 mb-3 font-weight-normal">Register</h1>
+          <div className="form-group">
+            <label htmlFor="name">
+              First Name
+              <input
+                type="text"
+                className="form-control"
+                name="firstname"
+                placeholder="Enter your first name"
+                value={firstname}
+                onChange={this.onChange}
+              />
+            </label>
           </div>
+          <div className="form-group">
+            <label htmlFor="name">
+              Last Name
+              <input
+                type="text"
+                className="form-control"
+                name="lastname"
+                placeholder="Enter your last name"
+                value={lastname}
+                onChange={this.onChange}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label htmlFor="username">
+              Username
+              <input
+                type="email"
+                className="form-control"
+                name="username"
+                placeholder="Enter username"
+                value={username}
+                onChange={this.onChange}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">
+              Password
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={this.onChange}
+              />
+            </label>
+          </div>
+          <button type="submit" className="btn btn-lg btn-primary btn-block">
+            Register!
+          </button>
+        </form>
+      </div>
     );
   }
 }
