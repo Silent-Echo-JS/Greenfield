2e98fe25d429221213bf2f9eacfc5a170c8deaf3
Dan Murphy <dmurphy79@gmail.com>
2019-09-16 16:00:34 -0500

    Adds state to App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,28 +1,34 @@
-import React from 'react';
-import { Route, HashRouter } from 'react-router-dom';
-import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
-import Navbar from './HeaderComponent/Navbar.jsx'; 
-import Dashboard from './DashboardComponents/Dashboard.jsx';
-import Login from './Auth/Login.jsx'
-import About from './HomePages/About.jsx'
-import Deposit from './Deposit.jsx';
-import Expense from './Expense.jsx';
-import Tenants from './Tenants.jsx';
-import Board from './Board.jsx';
-import Settings from './Settings.jsx';
-import NavBar from './HeaderComponent/Navbar.jsx';
+import React from "react";
+import { Route, HashRouter } from "react-router-dom";
+import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
+import Navbar from "./HeaderComponent/Navbar.jsx";
+import Dashboard from "./DashboardComponents/Dashboard.jsx";
+import Login from "./Auth/Login.jsx";
+import About from "./HomePages/About.jsx";
+import Deposit from "./Deposit.jsx";
+import Expense from "./Expense.jsx";
+import Tenants from "./Tenants.jsx";
+import Board from "./Board.jsx";
+import Settings from "./Settings.jsx";
+import NavBar from "./HeaderComponent/Navbar.jsx";
 
 function onAuthRequired({ history }) {
-  history.push('/login');
+  history.push("/login");
 }
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
       <HashRouter>
-        <Security issuer='https://dev-785992.okta.com/oauth2/default'
-          clientId='0oa1cspbxnLfDPYQb357'
-          redirectUri={window.location.origin + '/implicit/callback'}
+        <Security
+          issuer="https://dev-785992.okta.com/oauth2/default"
+          clientId="0oa1cspbxnLfDPYQb357"
+          redirectUri={window.location.origin + "/implicit/callback"}
           onAuthRequired={onAuthRequired}
         >
           {/* render the navbar when a user is not logged in and Dashboard when user is logged in */}
@@ -34,10 +40,17 @@ class App extends React.Component {
           <Route path="/Tenants" component={Tenants} />
           <Route path="/Board" component={Board} />
           <Route path="/Settings" component={Settings} />
-          
+
           <SecureRoute path="/" exact={true} component={Dashboard} />
-          <Route path='/login' render={() => <Login baseUrl='https://dev-785992.okta.com' />} />
-          <Route path='/implicit/callback' exact={true} component={ImplicitCallback} />
+          <Route
+            path="/login"
+            render={() => <Login baseUrl="https://dev-785992.okta.com" />}
+          />
+          <Route
+            path="/implicit/callback"
+            exact={true}
+            component={ImplicitCallback}
+          />
         </Security>
       </HashRouter>
     );
