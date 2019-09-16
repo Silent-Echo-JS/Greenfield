07fd87ee14a43936d97c688dc96d1673b04dbbcc
May <may0000ff@gmail.com>
2019-09-16 01:06:13 -0500

    removed routes in navbar

--- client/src/components/HeaderComponent/Navbar.jsx
+++ client/src/components/HeaderComponent/Navbar.jsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Login from '../HomePages/Login.jsx';
-import About from '../HomePages/About.jsx';
-import Register from '../HomePages/Register.jsx';
+import {Link} from 'react-router-dom'
+// import { HashLink as Link} from "react-router-hash-link";
 
 class NavBar extends React.Component {
   render() {
     return (
-      <Router>
-      <div class='title'><h1>Simple HOA</h1></div>
+      <div class='title'>
+        <h1>Simple HOA</h1>
         <div class='header'>
           <ul class="headerButtons">
-            <li className="navButton"><Link to="About">About</Link></li>
-            <li className="navButton"><Link to="">Home</Link></li>
+            <li className="navButton"><Link to="/about">About</Link></li>
+            <li className="navButton"><Link to="/">Home</Link></li>
           </ul>
         </div>
-        <Route name="login" exact path="/" component={Login} />
-        <Route name="about" exact path="/About" component={About} />
-        <Route name="Register" exact path="/Register" component={Register} />
-      </Router>
+      </div>
     )
   }
 }
