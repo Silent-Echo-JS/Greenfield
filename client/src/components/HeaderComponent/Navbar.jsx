705a53a912bc201d658322919b308cff090308f5
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-09-17 17:51:24 -0500

    commented out okta code

--- client/src/components/HeaderComponent/Navbar.jsx
+++ client/src/components/HeaderComponent/Navbar.jsx
@@ -4,10 +4,10 @@ import {Link} from 'react-router-dom'
 class NavBar extends React.Component {
   render() {
     return (
-      <div class='title'>
+      <div className='title'>
         <h1>Simple HOA</h1>
-        <div class='header'>
-          <ul class="headerButtons">
+        <div className='header'>
+          <ul className="headerButtons">
             <li className="navButton"><Link to="/">Home</Link></li>
             <li className="navButton"><Link to="/about">About</Link></li>
           </ul>
