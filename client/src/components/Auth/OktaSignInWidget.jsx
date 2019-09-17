4f8955eb769b2bf2613a00e92f2f977ee293bbd9
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-09-16 19:40:51 -0500

    commented back in auth

--- client/src/components/Auth/OktaSignInWidget.jsx
+++ client/src/components/Auth/OktaSignInWidget.jsx
@@ -1,28 +1,28 @@
-// import React, { Component } from 'react';
-// import ReactDOM from 'react-dom';
-// import OktaSignIn from '@okta/okta-signin-widget';
-// // import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import OktaSignIn from '@okta/okta-signin-widget';
+// import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
 
-// export default class OktaSignInWidget extends Component {
-//   componentDidMount() {
-//     const el = ReactDOM.findDOMNode(this);
-//     this.widget = new OktaSignIn({
-//       baseUrl: this.props.baseUrl,
-//       authParams: {
-//         pkce: true
-//       },
-//       features: {
-//         registration: true
-//       }
-//     });
-//     this.widget.renderEl({ el }, this.props.onSuccess, this.props.onError);
-//   }
+export default class OktaSignInWidget extends Component {
+  componentDidMount() {
+    const el = ReactDOM.findDOMNode(this);
+    this.widget = new OktaSignIn({
+      baseUrl: this.props.baseUrl,
+      authParams: {
+        pkce: true
+      },
+      features: {
+        registration: true
+      }
+    });
+    this.widget.renderEl({ el }, this.props.onSuccess, this.props.onError);
+  }
 
-//   componentWillUnmount() {
-//     this.widget.remove();
-//   }
+  componentWillUnmount() {
+    this.widget.remove();
+  }
 
-//   render() {
-//     return <div />;
-//   }
-// };
+  render() {
+    return <div />;
+  }
+};
