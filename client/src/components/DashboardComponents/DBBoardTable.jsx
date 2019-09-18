02776505e2750cf199b61c0c5235458cec613c05
sdelafuente12 <s.delafuente831@gmail.com>
2019-09-17 19:28:14 -0500

    ;}

--- client/src/components/DashboardComponents/DBBoardTable.jsx
+++ client/src/components/DashboardComponents/DBBoardTable.jsx
@@ -6,6 +6,8 @@ const DBBoardTable = () => {
     <div className="dashboard-card p-4 mb-4" Style="height:400px width: 100%">
       <h5>HOA Board</h5>
       <hr />
+      <h6> <b>Daddy</b></h6>
+      <p>Samantha De La Fuente</p>
       <h6>President</h6>
       <p>Dan Murphy</p>
       <hr />
@@ -15,8 +17,6 @@ const DBBoardTable = () => {
       <h6>Treasurer</h6>
       <p>Geoffrey Ian Ward</p>
       <hr />
-      <h6>Secretary</h6>
-      <p>Samantha De La Fuente</p>
       <hr />
       <Button size="sm">Edit Board</Button>
     </div>
