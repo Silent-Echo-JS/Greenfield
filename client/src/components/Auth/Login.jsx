8f758a23e4deda44e9a1d0ab6e515292c999e934
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-09-19 15:10:32 -0500

    extracted handleSubmit function out so login routing isn't working for now

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -1,41 +1,60 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import Axios from 'axios';
 import firebase from './firebase';
 
 export default class Login extends Component {
-
-
-  handleClick() {
-    firebase.loginWithGoogle()
-      .then((data) => {
-        console.log('HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII!!!!!!!!!!!!!!!!!!!!!!!!1', data);
-        // data also has idToken and sessionToken properties
-        const firebaseId = data.user.uid;
-        console.log('--------------------------fireBasedID', firebaseId);
-        localStorage.setItem('uid', firebaseId)
-        axios.get(`/checkForUser/${firebaseId}`)
-          .then((res) => {
-            console.log("========data", res);
-            if (res.data.registered) {
-              this.props.history.push('/');
-            } else {
-              this.props.history.push('/InputInfo');
-            }
-          }).catch(err => {
-            console.error('Error checking user status', err);
-            // alert('Unable to Login User');
-          });
-      })
-      .catch((err) => {
-        console.error("=======error", err)
-      });
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
   }
 
+  handleClick() {
+    this.props.getHoaInfo();
+      // .then((res) => {
+      //   console.log("========data", res);
+      //   if (res.data.registered) {
+      //     this.props.history.push('/');
+      //   } else {
+      //     this.props.history.push('/InputInfo');
+      //   }
+      // }).catch(err => {
+      //   console.error('Error checking user status', err);
+      //   // alert('Unable to Login User');
+      // });
+}
+
+  // handleClick() {
+  //   firebase.loginWithGoogle()
+  //     .then((data) => {
+  //       console.log('HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII!!!!!!!!!!!!!!!!!!!!!!!!1', data);
+  //       // data also has idToken and sessionToken properties
+  //       const firebaseId = data.user.uid;
+  //       console.log('--------------------------fireBasedID', firebaseId);
+  //       localStorage.setItem('uid', firebaseId)
+  //       axios.get(`/checkForUser/${firebaseId}`)
+  //         .then((res) => {
+  //           console.log("========data", res);
+  //           if (res.data.registered) {
+  //             this.props.history.push('/');
+  //           } else {
+  //             this.props.history.push('/InputInfo');
+  //           }
+  //         }).catch(err => {
+  //           console.error('Error checking user status', err);
+  //           // alert('Unable to Login User');
+  //         });
+  //     })
+  //     .catch((err) => {
+  //       console.error("=======error", err)
+  //     });
+  // }
+
   render() {
+    console.log('LOGIN PROPS', this.props);
     return (
       <div>
         <h1>Login</h1>
-        <button onClick={this.handleClick.bind(this)}>Login with Google</button>
+        <button onClick={this.handleClick}>Login with Google</button>
       </div>
     )
   };
