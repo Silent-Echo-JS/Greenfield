b49c59cab4ab642abe7b673d27829bc6b24525cd
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-09-20 01:53:13 -0500

    passing hoaInfo from form to server and then App.jsx after a user signs-up or logs-in

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -8,49 +8,51 @@ export default class Login extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
-    this.props.getHoaInfo();
-      // .then(() => {
-      //   this.props.history.push('/');
-      // })
-      // .then((res) => {
-      //   console.log("========data", res);
-      //   if (res.data.registered) {
-      //     this.props.history.push('/');
-      //   } else {
-      //     this.props.history.push('/InputInfo');
-      //   }
-      // }).catch(err => {
-      //   console.error('Error checking user status', err);
-      //   // alert('Unable to Login User');
-      // });
-}
+//   handleClick() {
+//     this.props.getHoaInfo();
+//       // .then(() => {
+//       //   this.props.history.push('/');
+//       // })
+//       // .then((res) => {
+//       //   console.log("========data", res);
+//       //   if (res.data.registered) {
+//       //     this.props.history.push('/');
+//       //   } else {
+//       //     this.props.history.push('/InputInfo');
+//       //   }
+//       // }).catch(err => {
+//       //   console.error('Error checking user status', err);
+//       //   // alert('Unable to Login User');
+//       // });
+// }
 
-  // handleClick() {
-  //   firebase.loginWithGoogle()
-  //     .then((data) => {
-  //       console.log('HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII!!!!!!!!!!!!!!!!!!!!!!!!1', data);
-  //       // data also has idToken and sessionToken properties
-  //       const firebaseId = data.user.uid;
-  //       console.log('--------------------------fireBasedID', firebaseId);
-  //       localStorage.setItem('uid', firebaseId)
-  //       axios.get(`/checkForUser/${firebaseId}`)
-  //         .then((res) => {
-  //           console.log("========data", res);
-  //           if (res.data.registered) {
-  //             this.props.history.push('/');
-  //           } else {
-  //             this.props.history.push('/InputInfo');
-  //           }
-  //         }).catch(err => {
-  //           console.error('Error checking user status', err);
-  //           // alert('Unable to Login User');
-  //         });
-  //     })
-  //     .catch((err) => {
-  //       console.error("=======error", err)
-  //     });
-  // }
+  handleClick() {
+    firebase.loginWithGoogle()
+      .then((data) => {
+        // console.log('HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII!!!!!!!!!!!!!!!!!!!!!!!!1', data);
+        // data also has idToken and sessionToken properties
+        const firebaseId = data.user.uid;
+        // console.log('--------------------------fireBasedID', firebaseId);
+        localStorage.setItem('uid', firebaseId)
+        axios.get(`/checkForUser/${firebaseId}`)
+          .then((res) => {
+            console.log("========data", res.data);
+            if (res.data.registered) {
+              localStorage.setItem('hoaId', res.data.hoaInfoFromDb.id);
+              localStorage.setItem('hoaInfo', JSON.stringify(res.data.hoaInfoFromDb));
+              this.props.history.push('/');
+            } else {
+              this.props.history.push('/InputInfo');
+            }
+          }).catch(err => {
+            console.error('Error checking user status', err);
+            // alert('Unable to Login User');
+          });
+      })
+      .catch((err) => {
+        console.error("=======error", err)
+      });
+  }
 
   render() {
     console.log('LOGIN PROPS', this.props);
